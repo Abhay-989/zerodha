3db862394440d3b9a64edc44463fb416b451b250
Abhay-989 <abhaychouhan8799@gmail.com>
2025-11-29 10:41:04 +0530

    commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,7 +3,7 @@ import Order from "../models/OrdersModel.js";
 export const createOrder = async (req, res) => {
   try {
     const { name, qty, price, mode } = req.body;
-    const userId = req.user.id || req.user._id; // from auth middleware
+    const userId = req.user._id; // from auth middleware
 
     if (!name || !qty || !price || !mode) {
       return res.status(400).json({ success: false, message: "Missing fields" });
@@ -26,7 +26,7 @@ export const createOrder = async (req, res) => {
 
 export const getUserOrders = async (req, res) => {
   try {
-    const userId = req.user.id || req.user._id;
+    const userId = req.user._id;
     const orders = await Order.find({ userId }).sort({ createdAt: -1 });
 
     res.status(200).json(orders);
--- backend/index.js
+++ backend/index.js
@@ -252,6 +252,13 @@ app.post("/newOrder", async (req, res) => {
   }
 });
 
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
